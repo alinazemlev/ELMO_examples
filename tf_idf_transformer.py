b7a0e95469007e78f40e29bb7d4b5b695c699589
alinazemlev <alina.zemlev@gmail.com>
2020-02-10 10:38:08 +0300

    Add files via upload

---
+++ tf_idf_transformer.py
@@ -0,0 +1,35 @@
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+from collections import defaultdict
+
+
+# coding: utf-8
+
+# In[ ]:
+
+
+class TfidfEmbeddingVectorizer(object):
+    def __init__(self, word2vec):
+        self.word2vec = word2vec
+        self.word2weight = None
+        self.dim = len(next(iter(self.word2vec.values())))
+
+    def fit(self, X):
+        tfidf = TfidfVectorizer(analyzer=lambda x: x)
+        tfidf.fit(X)
+        max_idf = max(tfidf.idf_)
+        self.word2weight = defaultdict(
+            lambda: max_idf,
+            [(w, tfidf.idf_[i]) for w, i in tfidf.vocabulary_.items()])
+
+        return self
+
+    def transform(self, X):
+        return np.array([
+                np.mean([self.word2vec[w] * self.word2weight[w]
+                         for w in words if w in self.word2vec] or
+                        [np.zeros(self.dim)], axis=0)
+                for words in X
+            ])
+
